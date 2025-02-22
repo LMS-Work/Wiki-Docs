e127812883348da24adbe539438425be271369be
znc15 <64775575+znc15@users.noreply.github.com>
2025-02-22 17:58:04 +0800

    修复github page资源错误的问题

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Wiki-Docs/",
 
   lang: "zh-CN",
   title: "LittleSheep Minecraft Wiki",
